6b05358b3e8080d1b692d7d06475d901ae948341
Elivelton Gustavo <elivelton.gustavo34@gmail.com>
2020-12-11 15:22:26 -0300

    Integration Tests and Maven profile to tests - finished

--- src/main/java/com/example/springessentials/domain/Anime.java
+++ src/main/java/com/example/springessentials/domain/Anime.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
 
 @Data
 @Builder
--- src/main/java/com/example/springessentials/requests/AnimePostRequestBody.java
+++ src/main/java/com/example/springessentials/requests/AnimePostRequestBody.java
@@ -1,12 +1,16 @@
 package com.example.springessentials.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
     @NotEmpty(message = "O nome do anime não pode ser vazio")
     private String nome;
